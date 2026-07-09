39226b231c8fdcb10122773f47cad8ddd7b25f2a
Maksim2021-whiteHACKER <maksim25082002x5@gmail.com>
2026-07-10 00:29:54 +0400

    0.0.2.4: добавлено пояснение об актуальной версии игры

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,4 @@
+// Scripts/index.js
 import * as THREE from '../Scripts/lib/three.module.js';
 import Stats from 'https://unpkg.com/three@0.122.0/examples/jsm/libs/stats.module.js';
 import { OrbitControls } from '../Scripts/lib/OrbitControls.js';
